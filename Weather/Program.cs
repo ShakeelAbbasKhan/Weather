6234577f4291a81caf753ec561755ebdafecc149
ShakeelAbbasKhan <shakeelabbaskhan4455@gmail.com>
2023-11-06 20:32:49 +0500

    Add project files.

---
+++ Weather/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Weather
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var repository = new WeatherRepository(config);
+
+            Console.WriteLine("Welcome to the Open Weather API!");
+
+            while (true)
+            {
+                Console.WriteLine("\nSelect an option:");
+                Console.WriteLine("1. Get weather for a specific city");
+                Console.WriteLine("2. Exit");
+
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.WriteLine("Enter the city name:");
+                    string cityName = Console.ReadLine();
+
+                    Console.WriteLine("Enter the country code (optional, e.g., PK):");
+                    string countryCode = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(countryCode))
+                    {
+                        repository.GetWeatherForCity(cityName);
+                    }
+                    else
+                    {
+                        repository.GetWeatherForCity(cityName, countryCode);
+                    }
+                }
+                else if (choice == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please select a valid option.");
+                }
+            }
+        }
+   
+    }
+}
---
+++ Weather/WeatherRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace Weather
+{
+    public class WeatherRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _client;
+
+        public WeatherRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _client = new HttpClient();
+        }
+
+
+        public void GetWeatherForCity(string city, string countryCode = "PK")
+        {
+            string apiKey = _configuration["WeatherApi:ApiKey"];
+            string baseUrl = _configuration["WeatherApi:BaseUrl"];
+
+            var userURL = $"{baseUrl}weather?q={city},{countryCode}&appid={apiKey}&units=metric";
+
+            try
+            {
+                var response = _client.GetAsync(userURL).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var weatherResponse = response.Content.ReadAsStringAsync().Result;
+                    var temperature = JObject.Parse(weatherResponse);
+                    var formattedResponseMain = temperature["main"]["temp"].ToString();
+                    Console.WriteLine($"\n{city}, {countryCode}: {formattedResponseMain} degrees Centigrade.");
+                }
+                else
+                {
+                    Console.WriteLine($"Error: City '{city}' not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+    }
+}
